f69f5a9162bf297e2709987d5df4f6bd21571acc
Brandon Jones <jones.brandon@siemens.com>
2023-04-20 10:02:38 -0400

    Update Route Table

--- main.tf
+++ main.tf
@@ -3,19 +3,10 @@ resource "aws_subnet" "private_subnet" {
   cidr_block              = "10.0.224.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = "false"
-  tags {
-    Name = "brandon-private-sn"
-  }
 }
 
 resource "aws_route_table" "private" {
   vpc_id = data.aws_vpc.vpc.id
-  route {
-
-  }
-  tags = {
-    Name = "brandon-private-rt"
-  }
 }
 
 resource "aws_route_table_association" "private" {
